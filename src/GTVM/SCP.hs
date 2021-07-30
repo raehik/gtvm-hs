9746e149389eaac50952ece2f3df2305509f11f3
Ben Orchard <thefirstmuffinman@gmail.com>
2021-07-30 22:34:24 +0100

    complete simple parser

--- src/GTVM/SCP.hs
+++ src/GTVM/SCP.hs
@@ -21,11 +21,12 @@ data SCPSegment
   | SCPSeg03 Word8 Bytes Word8
   | SCPSeg04 Word8 Word8
   | SCPSeg05Textbox Word8 Word32 Bytes Bytes Word32
--- can't see an 0x06 case in code
+  -- no 0x06
   | SCPSeg07SCP Bytes
   | SCPSeg08
   | SCPSeg09 Word8 [(Bytes, Word32)]
   | SCPSeg0A Word8 Word8 Word32 Word32 Word32
+  -- ^ TODO: decomp code a bit confusing for this command, be aware
   | SCPSeg0B Word8 Word8
   | SCPSeg0CFlag Word8 Word8
   | SCPSeg0D Word8
@@ -41,31 +42,89 @@ data SCPSegment
   | SCPSeg17 Word8 Word8
   | SCPSeg18 Word8 Word8
   | SCPSeg19 Word8 Word8
+  -- no 0x1A
+  -- no 0x1B
+  -- no 0x1C
   | SCPSeg1D
   | SCPSeg1E Word8
   | SCPSeg1FDelay
   | SCPSeg20 Word8
   | SCPSeg21
+  | SCPSeg22 Bytes [(Bytes, Word32)]
+  | SCPSeg23SFX
+  | SCPSeg24 Bytes
+  | SCPSeg25
+  | SCPSeg26
+  | SCPSeg27 Bytes Word8 Word8
+  | SCPSeg28 Bytes Word8 Word8
+  | SCPSeg29 Bytes Word8 Word8
+  | SCPSeg2A Word8 Word8
+  | SCPSeg2B Word8 Word8 Word8 Bytes Word8
+  | SCPSeg2CMap
+  | SCPSeg2D Bytes Word8 Word8
+  | SCPSeg2E Word8 Word8 Word32 Word32
+  | SCPSeg2F Word8
   | SCPSeg30 Word8
   | SCPSeg31
   | SCPSeg32 Word32 Word8
+  | SCPSeg33 Word8 Word8
   | SCPSeg34 Word32
+  | SCPSeg35 Bytes
+  | SCPSeg36 Word8 Word32
+  | SCPSeg37 Word8 Word32
+  | SCPSeg38 Word8 Word32
+  | SCPSeg39
   | SCPSeg3A Word8 Word8
+  | SCPSeg3B Word8
   | SCPSeg3C Word8 Word8
+  | SCPSeg3D
+  | SCPSeg3E Word8 Word8
+  | SCPSeg3F Word8 Word32 Word32
+  | SCPSeg40 Word8
   | SCPSeg41 Word8 Word32 Word32
+  | SCPSeg42 Word8
+  -- these don't appear very SFXy in code, but did in data
   | SCPSeg43SFX Bytes
   | SCPSeg44SFX Bytes
   | SCPSeg45SFX Bytes Word8
   | SCPSeg46 Word8 Word8
+  | SCPSeg47 Word8 Word8
+  | SCPSeg48
+  | SCPSeg49
+  | SCPSeg4A
+  | SCPSeg4B
+  | SCPSeg4C Bytes
+  | SCPSeg4D
+  | SCPSeg4E
+  | SCPSeg4F Bytes Word8 Word8
+  | SCPSeg50 Bytes Word8 Word8
+  | SCPSeg51 Bytes Word8 Word8
+  | SCPSeg52 Bytes
+  | SCPSeg53 Bytes Word8 Word8
   | SCPSeg54 Word8 Word8
   | SCPSeg55 Word8 Word8
   | SCPSeg56 Word8 Word8
+  | SCPSeg57 Word8 Word8
+  | SCPSeg58 Word8 Word8
+  | SCPSeg59 Word8
+  | SCPSeg5A Word32 Word8
+  | SCPSeg5B Word32
   | SCPSeg5C Word8
   | SCPSeg5D Word32
   | SCPSeg5E Word32
   | SCPSeg5F Word32
   | SCPSeg60 Word32
+  | SCPSeg61 Word8 Word8
+  | SCPSeg62 Word8 -- 0x01 <= w8 <= 0x11, no default
+  | SCPSeg63
+  | SCPSeg64
+  | SCPSeg65Trophy
+  | SCPSeg66
+  | SCPSeg67
   | SCPSeg68
+  | SCPSeg69
+  | SCPSeg6A Word8
+  | SCPSeg6B Word8
   | SCPSeg6CWipe Word8 Word32 Word32 Word32
   | SCPSeg6DWipe Word8 Word32 Word32 Word32
   | SCPSeg6E
@@ -73,11 +132,11 @@ data SCPSegment
   | SCPSeg70 Word8 Word8
   | SCPSeg71 Word8 Word8
   | SCPSeg72
-  | SCPSeg73Kyoro Word8 Word32
+  | SCPSeg73Kyoro Word8 Word32 -- 0x01 <= w8 <= 0x06, with default
   | SCPSeg74
   | SCPSeg75 Word8
+  | SCPSeg76
   | SCPSeg77SCP Word8
-  -- ^ byte apparently should be between 1-6 inclusive
     deriving (Eq, Show)
 
 data SCPOpts = SCPOpts
--- src/GTVM/SCP/Parse.hs
+++ src/GTVM/SCP/Parse.hs
@@ -1,4 +1,7 @@
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE StandaloneDeriving         #-}
 
 module GTVM.SCP.Parse where
 
@@ -9,114 +12,200 @@ import qualified Data.ByteString as BS
 import           Data.Void
 import           Data.Word
 import           Data.List.NonEmpty ( NonEmpty(..) )
+import           Control.Monad.Reader
+import           Data.Either.Combinators ( mapLeft )
 
-type Parser = Parsec Void Bytes
+parseSCPFile :: MonadIO m => FilePath -> m (Either String [SCPSegment])
+parseSCPFile fp = do
+    bs <- liftIO $ BS.readFile fp
+    return $ parseSCPBytes' fp scpOptsDef bs
 
-pSCPSeg :: Parser SCPSegment
+parseSCPBytes :: Bytes -> Either String [SCPSegment]
+parseSCPBytes = parseSCPBytes' "" scpOptsDef
+
+parseSCPBytes' :: String -> SCPOpts -> Bytes -> Either String [SCPSegment]
+parseSCPBytes' fp opts bs =
+    mapLeft errorBundlePretty $ parse (runReaderT (many pSCPSeg) opts) fp bs
+
+pSCPSeg :: (MonadParsec Void Bytes m, MonadReader SCPOpts m) => m SCPSegment
 pSCPSeg = do
-    byte <- anyW8
+    byte <- pW8
     case byte of
       0x00 -> return SCPSeg00
-      0x01 -> SCPSeg01BG <$> pCString <*> anyW8 <*> anyW8
-      0x02 -> SCPSeg02SFX <$> pCString <*> anyW8
-      0x03 -> SCPSeg03 <$> anyW8 <*> pCString <*> anyW8
-      0x04 -> SCPSeg04 <$> anyW8 <*> anyW8
-      0x05 -> pSCPSeg05Textbox
-      -- can't see an 0x06 case in code
-      0x07 -> SCPSeg07SCP <$> pCString
+      0x01 -> SCPSeg01BG <$> pBytestring <*> pW8 <*> pW8
+      0x02 -> SCPSeg02SFX <$> pBytestring <*> pW8
+      0x03 -> SCPSeg03 <$> pW8 <*> pBytestring <*> pW8
+      0x04 -> SCPSeg04 <$> pW8 <*> pW8
+      0x05 -> SCPSeg05Textbox <$> pW8 <*> pW32 <*> pBytestring <*> pBytestring <*> pW32
+      -- no 0x06
+      0x07 -> SCPSeg07SCP <$> pBytestring
       0x08 -> return SCPSeg08
-      0x09 -> pSCPSeg09
-      0x0A -> pSCPSeg0A
-      0x0B -> SCPSeg0B <$> anyW8 <*> anyW8
-      0x0C -> SCPSeg0CFlag <$> anyW8 <*> anyW8
-      0x0D -> SCPSeg0D <$> anyW8
-      0x0E -> SCPSeg0E <$> anyW8
+      0x09 -> SCPSeg09 <$> pW8 <*> pCount pBSW32
+      0x0A -> SCPSeg0A <$> pW8 <*> pW8 <*> pW32 <*> pW32 <*> pW32
+      0x0B -> SCPSeg0B <$> pW8 <*> pW8
+      0x0C -> SCPSeg0CFlag <$> pW8 <*> pW8
+      0x0D -> SCPSeg0D <$> pW8
+      0x0E -> SCPSeg0E <$> pW8
       0x0F -> return SCPSeg0F
-      0x10 -> SCPSeg10 <$> anyW8 <*> anyW8 <*> anyW8
-      0x11 -> SCPSeg11EventCG <$> pCString
+      0x10 -> SCPSeg10 <$> pW8 <*> pW8 <*> pW8
+      0x11 -> SCPSeg11EventCG <$> pBytestring
       0x12 -> return SCPSeg12
-      0x13 -> SCPSeg13 <$> anyW8 <*> pCString <*> anyW8 <*> anyW32LE
-      0x14 -> SCPSeg14 <$> anyW8
+      0x13 -> SCPSeg13 <$> pW8 <*> pBytestring <*> pW8 <*> pW32
+      0x14 -> SCPSeg14 <$> pW8
       0x15 -> return SCPSeg15
       0x16 -> return SCPSeg16Wadai
-      0x17 -> SCPSeg17 <$> anyW8 <*> anyW8
-      0x18 -> SCPSeg18 <$> anyW8 <*> anyW8
-      0x19 -> SCPSeg19 <$> anyW8 <*> anyW8
+      0x17 -> SCPSeg17 <$> pW8 <*> pW8
+      0x18 -> SCPSeg18 <$> pW8 <*> pW8
+      0x19 -> SCPSeg19 <$> pW8 <*> pW8
+      -- no 0x1A
+      -- no 0x1B
+      -- no 0x1C
       0x1D -> return SCPSeg1D
-      0x1E -> SCPSeg1E <$> anyW8
+      0x1E -> SCPSeg1E <$> pW8
       0x1F -> return SCPSeg1FDelay
-      0x20 -> SCPSeg20 <$> anyW8
+      0x20 -> SCPSeg20 <$> pW8
       0x21 -> return SCPSeg21
-      -- TODO 0x22 -> SCPSeg22 <$>1
-      0x30 -> SCPSeg30 <$> anyW8
+      0x22 -> SCPSeg22 <$> pBytestring <*> pCount pBSW32
+      0x23 -> return SCPSeg23SFX
+      0x24 -> SCPSeg24 <$> pBytestring
+      0x25 -> return SCPSeg25
+      0x26 -> return SCPSeg26
+      0x27 -> SCPSeg27 <$> pBytestring <*> pW8 <*> pW8
+      0x28 -> SCPSeg28 <$> pBytestring <*> pW8 <*> pW8
+      0x29 -> SCPSeg29 <$> pBytestring <*> pW8 <*> pW8
+      0x2A -> SCPSeg2A <$> pW8 <*> pW8
+      0x2B -> SCPSeg2B <$> pW8 <*> pW8 <*> pW8 <*> pBytestring <*> pW8
+      0x2C -> return SCPSeg2CMap
+      0x2D -> SCPSeg2D <$> pBytestring <*> pW8 <*> pW8
+      0x2E -> SCPSeg2E <$> pW8 <*> pW8 <*> pW32 <*> pW32
+      0x2F -> SCPSeg2F <$> pW8
+      0x30 -> SCPSeg30 <$> pW8
       0x31 -> return SCPSeg31
-      0x32 -> SCPSeg32 <$> anyW32LE <*> anyW8
-      0x34 -> SCPSeg34 <$> anyW32LE
-      0x3A -> SCPSeg3A <$> anyW8 <*> anyW8
-      0x3C -> SCPSeg3C <$> anyW8 <*> anyW8
-      0x41 -> SCPSeg41 <$> anyW8 <*> anyW32LE <*> anyW32LE
-      0x43 -> SCPSeg43SFX <$> pCString
-      0x44 -> SCPSeg44SFX <$> pCString
-      0x45 -> SCPSeg45SFX <$> pCString <*> anyW8
-      0x46 -> SCPSeg46 <$> anyW8 <*> anyW8
-      0x54 -> SCPSeg54 <$> anyW8 <*> anyW8
-      0x55 -> SCPSeg55 <$> anyW8 <*> anyW8
-      0x56 -> SCPSeg56 <$> anyW8 <*> anyW8
-      0x5C -> SCPSeg5C <$> anyW8
-      0x5D -> SCPSeg5D <$> anyW32LE
-      0x5E -> SCPSeg5E <$> anyW32LE
-      0x5F -> SCPSeg5F <$> anyW32LE
-      0x60 -> SCPSeg60 <$> anyW32LE
+      0x32 -> SCPSeg32 <$> pW32 <*> pW8
+      0x33 -> SCPSeg33 <$> pW8 <*> pW8
+      0x34 -> SCPSeg34 <$> pW32
+      0x35 -> SCPSeg35 <$> pBytestring
+      0x36 -> SCPSeg36 <$> pW8 <*> pW32
+      0x37 -> SCPSeg37 <$> pW8 <*> pW32
+      0x38 -> SCPSeg38 <$> pW8 <*> pW32
+      0x39 -> return SCPSeg39
+      0x3A -> SCPSeg3A <$> pW8 <*> pW8
+      0x3B -> SCPSeg3B <$> pW8
+      0x3C -> SCPSeg3C <$> pW8 <*> pW8
+      0x3D -> return SCPSeg3D
+      0x3E -> SCPSeg3E <$> pW8 <*> pW8
+      0x3F -> SCPSeg3F <$> pW8 <*> pW32 <*> pW32
+      0x40 -> SCPSeg40 <$> pW8
+      0x41 -> SCPSeg41 <$> pW8 <*> pW32 <*> pW32
+      0x42 -> SCPSeg42 <$> pW8
+      0x43 -> SCPSeg43SFX <$> pBytestring
+      0x44 -> SCPSeg44SFX <$> pBytestring
+      0x45 -> SCPSeg45SFX <$> pBytestring <*> pW8
+      0x46 -> SCPSeg46 <$> pW8 <*> pW8
+      0x47 -> SCPSeg47 <$> pW8 <*> pW8
+      0x48 -> return SCPSeg48
+      0x49 -> return SCPSeg49
+      0x4A -> return SCPSeg4A
+      0x4B -> return SCPSeg4B
+      0x4C -> SCPSeg4C <$> pBytestring
+      0x4D -> return SCPSeg4D
+      0x4E -> return SCPSeg4E
+      0x4F -> SCPSeg4F <$> pBytestring <*> pW8 <*> pW8
+      0x50 -> SCPSeg50 <$> pBytestring <*> pW8 <*> pW8
+      0x51 -> SCPSeg51 <$> pBytestring <*> pW8 <*> pW8
+      0x52 -> SCPSeg52 <$> pBytestring
+      0x53 -> SCPSeg53 <$> pBytestring <*> pW8 <*> pW8
+      0x54 -> SCPSeg54 <$> pW8 <*> pW8
+      0x55 -> SCPSeg55 <$> pW8 <*> pW8
+      0x56 -> SCPSeg56 <$> pW8 <*> pW8
+      0x57 -> SCPSeg57 <$> pW8 <*> pW8
+      0x58 -> SCPSeg58 <$> pW8 <*> pW8
+      0x59 -> SCPSeg59 <$> pW8
+      0x5A -> SCPSeg5A <$> pW32 <*> pW8
+      0x5B -> SCPSeg5B <$> pW32
+      0x5C -> SCPSeg5C <$> pW8
+      0x5D -> SCPSeg5D <$> pW32
+      0x5E -> SCPSeg5E <$> pW32
+      0x5F -> SCPSeg5F <$> pW32
+      0x60 -> SCPSeg60 <$> pW32
+      0x61 -> SCPSeg61 <$> pW8 <*> pW8
+      0x62 -> SCPSeg62 <$> pW8
+      0x63 -> return SCPSeg63
+      0x64 -> return SCPSeg64
+      0x65 -> return SCPSeg65Trophy
+      0x66 -> return SCPSeg66
+      0x67 -> return SCPSeg67
       0x68 -> return SCPSeg68
-      0x6C -> SCPSeg6CWipe <$> anyW8 <*> anyW32LE <*> anyW32LE <*> anyW32LE
-      0x6D -> SCPSeg6DWipe <$> anyW8 <*> anyW32LE <*> anyW32LE <*> anyW32LE
+      0x69 -> return SCPSeg69
+      0x6A -> SCPSeg6A <$> pW8
+      0x6B -> SCPSeg6B <$> pW8
+      0x6C -> SCPSeg6CWipe <$> pW8 <*> pW32 <*> pW32 <*> pW32
+      0x6D -> SCPSeg6DWipe <$> pW8 <*> pW32 <*> pW32 <*> pW32
       0x6E -> return SCPSeg6E
       0x6F -> return SCPSeg6F
-      0x70 -> SCPSeg70 <$> anyW8 <*> anyW8
-      0x71 -> SCPSeg71 <$> anyW8 <*> anyW8
+      0x70 -> SCPSeg70 <$> pW8 <*> pW8
+      0x71 -> SCPSeg71 <$> pW8 <*> pW8
       0x72 -> return SCPSeg72
-      0x73 -> SCPSeg73Kyoro <$> anyW8 <*> anyW32LE
+      0x73 -> SCPSeg73Kyoro <$> pW8 <*> pW32
       0x74 -> return SCPSeg74
-      0x75 -> SCPSeg75 <$> anyW8
-      0x77 -> SCPSeg77SCP <$> anyW8
+      0x75 -> SCPSeg75 <$> pW8
+      0x76 -> return SCPSeg76
+      0x77 -> SCPSeg77SCP <$> pW8
       _    -> unexpected (Tokens (byte :| []))
 
-pSCPSeg05Textbox :: Parser SCPSegment
-pSCPSeg05Textbox = do
-    bUnk          <- anyW8
-    speakerId     <- anyW32LE
-    text          <- pCString
-    voicePath     <- pCString
-    counterUnk    <- anyW32LE
-    return $ SCPSeg05Textbox bUnk speakerId text voicePath counterUnk
+-- | Parse a single byte, then apply a parser that many times.
+pCount :: (MonadParsec e s m, Token s ~ Word8) => m a -> m [a]
+pCount p = pW8 >>= \i -> count (fromIntegral i) p
 
-pSCPSeg09 :: Parser SCPSegment
-pSCPSeg09 = do
-    bUnk    <- anyW8
-    counter <- anyW8
-    values  <- count (fromIntegral counter) ((,) <$> pCString <*> anyW32LE)
-    return $ SCPSeg09 bUnk values
+-- | Parse a bytestring, followed by a 'Word32'.
+pBSW32 :: (MonadParsec e Bytes m, MonadReader SCPOpts m) => m (Bytes, Word32)
+pBSW32 = (,) <$> pBytestring <*> pW32
 
--- TODO: Nasty one. Looks like the Word32s are used to calculate something.
-pSCPSeg0A :: Parser SCPSegment
-pSCPSeg0A = SCPSeg0A <$> anyW8 <*> anyW8 <*> anyW32LE <*> anyW32LE <*> anyW32LE
+-- | Parse a bytestring.
+--
+-- The type of bytestring to parse is selected using provided context.
+pBytestring :: (MonadParsec e Bytes m, MonadReader SCPOpts m) => m Bytes
+pBytestring = reader scpOptStringType >>= \case
+  StrTyCString      -> pCString
+  StrTyLengthPrefix -> pPascal
+
+-- | Parse a null-terminated bytestring (a C string), consuming the null.
+pCString :: (MonadParsec e Bytes m) => m Bytes
+pCString =
+    takeWhileP (Just "non-null byte") (/= 0x00) <* pNullByte <?> "null-terminated bytestring"
+
+-- | Parse the null byte.
+pNullByte :: (MonadParsec e s m, Token s ~ Word8) => m Word8
+pNullByte = single 0x00 <?> "null byte"
+
+-- | Parse a length-prefixed bytestring (a Pascal string).
+pPascal :: (MonadParsec e BS.ByteString m) => m BS.ByteString
+pPascal = do
+    len <- pW8
+    let lbl = show len <> "-byte bytestring"
+    takeP (Just lbl) (fromIntegral len)
+
+-- | Parse any byte.
+pW8 :: (MonadParsec e s m, Token s ~ Word8) => m Word8
+pW8 = anyW8
+
+-- | Parse any 'Word32'.
+--
+-- Endianness to parse is selected using provided context.
+pW32 :: (MonadParsec e Bytes m, MonadReader SCPOpts m) => m Word32
+pW32 = caseEndianness anyW32LE anyW32BE
+
+--------------------------------------------------------------------------------
+
+-- | Helper for switching functions based on context endianness.
+caseEndianness :: MonadReader SCPOpts m => m a -> m a -> m a
+caseEndianness fLE fBE = reader scpOptEndianness >>= \case
+  LittleEndian -> fLE
+  BigEndian    -> fBE
+
+--------------------------------------------------------------------------------
 
 pWrap
     :: (MonadParsec e s m, Token s ~ Word8)
     => m a -> m (Either Word8 a)
 pWrap p = (Right <$> try p) <|> (Left <$> anySingle)
-
--- | Parse a null-terminated bytestring (a C-style string), consuming the null.
-pCString :: Parser Bytes
-pCString =
-    takeWhileP (Just "non-null byte") (/= 0x00) <* pNullByte <?> "null-terminated bytestring"
-
-pNullByte :: Parser Word8
-pNullByte = single 0x00 <?> "null byte"
-
---------------------------------------------------------------------------------
-
-parseSCPFile :: FilePath -> IO ()
-parseSCPFile fp = do
-    scpBytes <- BS.readFile fp
-    parseTest (many pSCPSeg) scpBytes
