4c53a3f2c49949525c96efcfa0e4ef7ed2916d00
Ben Orchard <thefirstmuffinman@gmail.com>
2024-10-01 17:38:33 +0100

    build tweaks

--- flake.nix
+++ flake.nix
@@ -5,11 +5,14 @@
     haskell-flake.url = "github:srid/haskell-flake";
     binrep.url   = "github:raehik/binrep";
     binrep.flake = false;
+    rerefined.url   = "github:raehik/rerefined";
+    rerefined.flake = false;
     bytezap.url   = "github:raehik/bytezap";
     bytezap.flake = false;
     strongweak.url   = "github:raehik/strongweak";
     strongweak.flake = false;
   };
+
   outputs = inputs:
   let
     defDevShell = compiler: {
@@ -22,6 +25,7 @@
       };
     };
   in
+
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = inputs.nixpkgs.lib.systems.flakeExposed;
       imports = [ inputs.haskell-flake.flakeModule ];
@@ -30,23 +34,63 @@
         devShells.default = self'.devShells.ghc96;
         haskellProjects.ghc910 = {
           basePackages = pkgs.haskell.packages.ghc910;
+          devShell = defDevShell "ghc910";
+
           # v https://github.com/phadej/defun/pull/5
           settings.defun-core.jailbreak = true;
+          packages.finite-typelits.source = "0.2.1.0";
+          packages.vector-sized.source = "1.6.1";
+
+          # 0.1.1.3 is last version before backpack overhaul
+          # Nix doesn't support backpack
+          packages.lzo.source = "0.1.1.3";
+          # 2023-02-20: tests broken b/c a file got left out of sdist
+          # https://hub.darcs.net/vmchale/sak/issue/2 (unmerged 2024-09-28)
+          settings.lzo.check = false;
+
+          #settings.text-icu.check = false; # 2024-09-25: one test fails???
+
+          packages.binrep.source = inputs.binrep;
+
+          # waiting for nixpkgs update
+          packages.rerefined.source = inputs.rerefined;
           settings.strongweak.broken = false;
-          settings.text-icu.check = false; # 2025-09-25: one test fails???
+          packages.strongweak.source = inputs.strongweak;
           packages.bytezap.source = inputs.bytezap;
-          devShell = defDevShell "ghc98";
         };
+
         haskellProjects.ghc98 = {
           basePackages = pkgs.haskell.packages.ghc98;
-          devShell = defDevShell "ghc98";
+          #devShell = defDevShell "ghc98";
+          devShell = compiler: {
+            mkShellArgs.name = "ghc98";
+            hoogle = false;
+            tools = _: {
+              cabal-install = pkgs.cabal-install; # TODO TMP
+              haskell-language-server = null;
+              hlint = null;
+              ghcid = null;
+            };
+          };
 
-          # 2024-09-28 raehik: broken due to blake3-0.3 not working (idk why)
+          # 2024-10-01 raehik: broken due to blake3-0.3 not working (idk why)
 
+          # 0.1.1.3 is last version before backpack overhaul
+          # Nix doesn't support backpack
+          packages.lzo.source = "0.1.1.3";
+          # 2023-02-20: tests broken b/c a file got left out of sdist
+          # https://hub.darcs.net/vmchale/sak/issue/2 (unmerged 2024-09-28)
+          settings.lzo.check = false;
+
+          packages.binrep.source = inputs.binrep;
+
+          # waiting for nixpkgs update
+          packages.rerefined.source = inputs.rerefined;
           settings.strongweak.broken = false;
-          settings.text-icu.check = false; # 2025-09-25: one test fails???
+          packages.strongweak.source = inputs.strongweak;
           packages.bytezap.source = inputs.bytezap;
         };
+
         haskellProjects.ghc96 = {
           basePackages = pkgs.haskell.packages.ghc96;
           devShell = defDevShell "ghc96";
@@ -58,9 +102,12 @@
           # https://hub.darcs.net/vmchale/sak/issue/2 (unmerged 2024-09-28)
           settings.lzo.check = false;
 
-          settings.strongweak.broken = false;
           packages.binrep.source = inputs.binrep;
+
+          # waiting for nixpkgs update
+          packages.rerefined.source = inputs.rerefined;
           packages.bytezap.source = inputs.bytezap;
+          settings.strongweak.broken = false;
           packages.strongweak.source = inputs.strongweak;
         };
 
